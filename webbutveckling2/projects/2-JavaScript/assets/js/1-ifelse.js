e75833c2af45f3fe3936f9998c496555afbeb4e7
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2020-09-18 15:10:35 +0200

    started with js

---
+++ webbutveckling2/projects/2-JavaScript/assets/js/1-ifelse.js
@@ -0,0 +1,24 @@
+// greater than
+var age = 18
+if (age >= 18) {
+    console.log('You can get your drivers licens.');
+} else {
+    console.log('you cannot get your drivers license');
+}
+
+// if something is equal too
+var lesson = 'Web Development';
+if (lesson == 'History') {
+    console.log('We have: ' + lesson);
+} else if (lesson == 'Web Development') {
+    console.log('We have: ' + lesson);
+} else {
+    console.log('Spare time!');
+}
+
+var isSunny = false;
+if (isSunny == true) {
+    console.log('Put on some shorts today');
+} else {
+    console.log('Put on some warm clothes today');
+}
