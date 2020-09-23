7040e4ba2c9ec71535c009bea9bc06a9a30d16ee
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2020-09-23 11:58:06 +0200

    continued with js

--- webbutveckling2/projects/2-JavaScript/assets/js/1-ifelse.js
+++ webbutveckling2/projects/2-JavaScript/assets/js/1-ifelse.js
@@ -22,3 +22,37 @@ if (isSunny == true) {
 } else {
     console.log('Put on some warm clothes today');
 }
+// funktioner i ett objekt är en metod
+// funktion = xxx()
+// metod = yy.xxx()
+
+// step 1
+var d = new Date();
+
+if (d.getUTCHours() < 18) {
+    console.log("G'Day!");
+} else {
+    console.log('Evening!');
+} 
+
+// step 2
+if (d.getUTCHours() < 10) {
+    console.log('Good morning!');
+} else if (d.getUTCHours() < 18) {
+    console.log("G'Day!");
+} else {
+    console.log("Good Evnin'");
+}
+
+// step 3
+if (d.getHours() < 10) {
+    if (d.getMonth() > 9 || d.getMonth() < 2) {
+        console.log('Go ahead and sleep like a bear!');
+    } else {
+        console.log('Good Morning!');
+    }
+} else if (d.getHours() < 18) {
+    console.log("G'Day!");
+} else {
+    console.log("Good Evnin'");
+}
