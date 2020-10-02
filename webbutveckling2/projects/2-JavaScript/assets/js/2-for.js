c800934a9e8078b603999b027ac7410a91544c69
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2020-10-02 15:09:56 +0200

    continued with for loops

--- webbutveckling2/projects/2-JavaScript/assets/js/2-for.js
+++ webbutveckling2/projects/2-JavaScript/assets/js/2-for.js
@@ -32,16 +32,18 @@ for (var i = 0; i < len; i++) {
 
 document.writeln("--------------------<br>");
 
-// The modulo operator returns remainder of devision
-// != means not equal to 
-// !=== means not equal to AND CHECK IF SAME DATA TYPE
-// == means equal to (12 === '12')
-// === means equal to AND CHACK IF SAME DATA TYPE (12 === '12')
-for (var i = 1; i <= 365; i++) {
-    if (i % 2 == 0) {
-        document.writeln('Even dates: ' + i + '<br>');
-    }
-}
+// // The modulo operator returns remainder of devision
+// // != means not equal to 
+// // !=== means not equal to AND CHECK IF SAME DATA TYPE
+// // == means equal to (12 === '12')
+// // === means equal to AND CHACK IF SAME DATA TYPE (12 === '12')
+// for (var i = 1; i <= 365; i++) {
+//     if (i % 2 === 0) {
+//         document.writeln('Even dates: ' + i + '<br>');
+//     }
+// }
+
+document.writeln('Here rests the one and only, even of dates.<br>')
 
 document.writeln("--------------------<br>");
 
@@ -53,28 +55,67 @@ document.writeln("--------------------<br>");
 // Print all numbers between -10 and 19
 
 for (var num = -10; num <= 19; num++) {
-    document.writeln(num + '<br>');
+    document.writeln(num);
 }
 
-document.writeln("--------------------<br>");
+document.writeln("<br>--------------------<br>");
 
 // Exercise 4
 // Print even numbers between 10 and 40
 
-for (var num = 10; num <= 40; i++) {
+for (var num = 10; num <= 40; num++) {
     if (num % 2 === 0) {
-        document.writeln(num + '<br>');
+        document.writeln(num);
     }
 }
 
-document.writeln("--------------------<br>");
+document.writeln("<br>--------------------<br>");
 
 // Exercise 5
 // print odd between 300 and 333
 
-document.writeln("--------------------<br>");
+for (var num = 300; num <= 333; num++) {
+    if (num % 2 !== 0) {
+        document.writeln(num);
+    }
+}
+
+document.writeln("<br>--------------------<br>");
 
 // Exercise 6
 // even num divideable by 5 And 3 between 5 och 50
 
+for (var num = 5; num <= 50; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        document.writeln(num)
+    }
+}
+
+document.writeln("<br>--------------------<br>");
+
+// Exercise 7 : multiplication table
+// Write a for loop that iterates from 0 to 10 
+// for each iteration of the loop it will multiply
+// the number by 9 and log the result
+
+var multi = 9;
+for (var num = 0; num <= 10; num++) {
+    var result = num * multi;
+    document.writeln(num + ' * ' + multi + ' = ' + result + '<br>');
+}
+
 document.writeln("--------------------<br>");
+
+// NESTED FOR LOOPS
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
+
+for (var i = 0; i < 5; i++) {
+    for (var j = 0; j < 5; j++) {
+        document.writeln('*');
+    }
+    document.writeln('<br>');
+}
