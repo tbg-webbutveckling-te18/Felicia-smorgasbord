09ddb24a70414f187746f3f282957bf9c85217de
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2020-10-16 15:08:51 +0200

    continued with functions in js

--- webbutveckling2/projects/2-JavaScript/assets/js/3-functions.js
+++ webbutveckling2/projects/2-JavaScript/assets/js/3-functions.js
@@ -28,7 +28,13 @@ twinkle();
 
 // document.writeln('<br>' + twinkle);
 
-document.writeln("<br>----------------------------------<br>" + ' <br>');
+/*
+******************************************************
+                      ARGUMENTS                       
+******************************************************
+*/
+
+document.writeln("----------------------------------<br>" + ' <br>');
 
 function square(num) {
     document.writeln(num * num);
@@ -37,6 +43,12 @@ function square(num) {
 square(10);
 square(5);
 square('h'); // not a number
+
+/*
+******************************************************
+                        RETURN                       
+******************************************************
+*/
 
 document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
 
@@ -57,6 +69,46 @@ function quadrupleMe(x) {
 }
 
 var myNum = quadrupleMe(4);
-document.writeln(myNum);
+
+// Select an element
+var btnNum = document.getElementById('myNum');
+
+// Manupilate an element
+btnNum.innerHTML = 'My <strong>favorite</strong>  number is; ' + myNum;
+
+
+
+// Funktion för att göra första bokstave stor
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+     
+}
+
+var city = 'stockholm';
+var capital = capitalize(city);
+document.writeln('The Capital of Sweden is: ' + capital);
+
+document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
+
+function isEven(x) {
+    if (num % 2 === 0) {
+        return document.writeln('True')
+    }
+    else {
+        return document.writeln('False')
+    }
+}
+
+var num = 4;
+isEven(num);
+
+document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
+
+function kebabToSnake(x) {
+    return document.writeln(x.replaceAll('-', '_'));
+}
+
+var word = 'Hello-there-friend';
+kebabToSnake(word);
 
 document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
