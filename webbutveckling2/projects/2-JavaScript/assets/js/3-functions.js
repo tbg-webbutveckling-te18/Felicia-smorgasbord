64960a68869adab15876fc357543c72e0c6009ab
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2020-11-04 11:55:16 +0100

    tellFortune

--- webbutveckling2/projects/2-JavaScript/assets/js/3-functions.js
+++ webbutveckling2/projects/2-JavaScript/assets/js/3-functions.js
@@ -14,138 +14,138 @@
 
 // document.writeln("<br>----------------------------------<br>" + ' <br>');
 
-function twinkle() {
-    document.writeln('Twinkle, twinkle, little star <br>');
-    document.writeln('how i wonder what you are <br>');
-    document.writeln('up above the world so high <br>');
-    document.writeln('like a diamond in the sky <br>');
+// function twinkle() {
+//     document.writeln('Twinkle, twinkle, little star <br>');
+//     document.writeln('how i wonder what you are <br>');
+//     document.writeln('up above the world so high <br>');
+//     document.writeln('like a diamond in the sky <br>');
 
-    document.writeln('<br>');
-}
+//     document.writeln('<br>');
+// }
 
-// twinkle();
-// twinkle();
+// // twinkle();
+// // twinkle();
 
-// document.writeln('<br>' + twinkle);
+// // document.writeln('<br>' + twinkle);
 
-/*
-******************************************************
-                      ARGUMENTS                       
-******************************************************
-*/
+// /*
+// ******************************************************
+//                       ARGUMENTS                       
+// ******************************************************
+// */
 
-document.writeln("----------------------------------<br>" + ' <br>');
+// document.writeln("----------------------------------<br>" + ' <br>');
 
-function square(num) {
-    document.writeln(num * num);
-}
+// function square(num) {
+//     document.writeln(num * num);
+// }
 
-square(10);
-square(5);
-square('h'); // not a number
+// square(10);
+// square(5);
+// square('h'); // not a number
 
-/*
-******************************************************
-                        RETURN                       
-******************************************************
-*/
+// /*
+// ******************************************************
+//                         RETURN                       
+// ******************************************************
+// */
 
-document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
+// document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
 
-function area(width, height) {
-    return width * height;
-}
+// function area(width, height) {
+//     return width * height;
+// }
 
-document.writeln('The area is: ' + area(5, 10));
+// document.writeln('The area is: ' + area(5, 10));
 
-var res = area(6, 9);
+// var res = area(6, 9);
 
-document.writeln(res);
+// document.writeln(res);
 
-document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
+// document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
 
-function quadrupleMe(x) {
-    return 4 * x;
-}
+// function quadrupleMe(x) {
+//     return 4 * x;
+// }
 
-var myNum = quadrupleMe(4);
+// var myNum = quadrupleMe(4);
 
-// Select an element
-var btnNum = document.getElementById('myNum');
+// // Select an element
+// var btnNum = document.getElementById('myNum');
 
-// Manupilate an element
-btnNum.innerHTML = 'My <strong>favorite</strong>  number is; ' + myNum;
+// // Manupilate an element
+// btnNum.innerHTML = 'My <strong>favorite</strong>  number is; ' + myNum;
 
 
 
 // Funktion för att göra första bokstave stor
-function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+// function capitalize(str) {
+//     return str.charAt(0).toUpperCase() + str.slice(1);
      
-}
+// }
 
-var city = 'stockholm';
-var capital = capitalize(city);
-document.writeln('The Capital of Sweden is: ' + capital);
-
-document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
-
-function isEven(x) {
-    if (num % 2 === 0) {
-        return document.writeln('True')
-    }
-    else {
-        return document.writeln('False')
-    }
-}
-
-var num = 4;
-isEven(num);
-
-document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
-
-function kebabToSnake(x) {
-    return document.writeln(x.replaceAll('-', '_'));
-}
-
-var word = 'Hello-there-friend';
-kebabToSnake(word);
-
-document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
-
-var p = document.getElementById('scopes'); // global scope
-
-// declare
-function localScope() {
-    var localVar = 31;
-    p.innerHTML = localVar;
-}
-
-// call
-localScope();
+// var city = 'stockholm';
+// var capital = capitalize(city);
+// document.writeln('The Capital of Sweden is: ' + capital);
 
 // document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
 
-var globalVar = 'This is a global var';
+// function isEven(x) {
+//     if (num % 2 === 0) {
+//         return document.writeln('True')
+//     }
+//     else {
+//         return document.writeln('False')
+//     }
+// }
 
-p.innerHTML = globalVar;
-p.style.fontSize = '20px';
-p.style.color = 'lightGreen';
+// var num = 4;
+// isEven(num);
 
-function changeGlobalVar() {
-    globalVar = 'Hah! I got a new value now!';
-    p.innerHTML = globalVar;
-}
+// document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
 
-changeGlobalVar();
+// function kebabToSnake(x) {
+//     return document.writeln(x.replaceAll('-', '_'));
+// }
 
-function tricky() {
-    var globalVar = 'Am i a local variable?';
-    p.innerHTML = globalVar;
-}
+// var word = 'Hello-there-friend';
+// kebabToSnake(word);
 
-tricky();
-p.innerHTML = globalVar;
+// document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
+
+// var p = document.getElementById('scopes'); // global scope
+
+// // declare
+// function localScope() {
+//     var localVar = 31;
+//     p.innerHTML = localVar;
+// }
+
+// // call
+// localScope();
+
+// // document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
+
+// var globalVar = 'This is a global var';
+
+// p.innerHTML = globalVar;
+// p.style.fontSize = '20px';
+// p.style.color = 'lightGreen';
+
+// function changeGlobalVar() {
+//     globalVar = 'Hah! I got a new value now!';
+//     p.innerHTML = globalVar;
+// }
+
+// changeGlobalVar();
+
+// function tricky() {
+//     var globalVar = 'Am i a local variable?';
+//     p.innerHTML = globalVar;
+// }
+
+// tricky();
+// p.innerHTML = globalVar;
 
 // document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
 
@@ -170,3 +170,27 @@ btnStopTime.addEventListener('click', function() {
 });
 
 // document.writeln(' <br> ' + '<br>----------------------------------<br>' + ' <br>');
+
+var output = document.getElementById('tellFortune');
+var btnFortune = document.getElementsByTagName('button')[1];
+
+function tellFortune(job, place, partner, kids) {
+    output.innerHTML = `You will be a/an ${job} in ${place} and married to ${partner} with ${kids} kid/kids`;
+    // var str = 'You will be a/an ' + x + ' in ' + y + ', and married to ' + z + ' with' + z + ' kidz.';
+}
+
+// tellFortune('Game Designer', 'New Yourk', 'Mei', 1);
+
+var jobArr = ['Game Designer', 'Web Developer', 'Homeless', 'YouTuber', 'President'];
+var placeArr = ['Tokyo', 'Moscow', 'New York', 'Manchester', 'Dublin'];
+var partnerArr = ['Mei', 'Mark', 'Johnny', 'Daichi', 'Igor'];
+var kidsArr = [0, 1, 2, 3, 8];
+
+btnFortune.addEventListener('click', function() {
+    var randomJob = jobArr[Math.floor(Math.random() * jobArr.length)];
+    var randomPlace = placeArr[Math.floor(Math.random() * placeArr.length)];
+    var randomPartner = partnerArr[Math.floor(Math.random() * partnerArr.length)];
+    var randomKids = kidsArr[Math.floor(Math.random() * kidsArr.length)];
+
+    tellFortune(randomJob, randomPlace, randomPartner, randomKids);
+});
