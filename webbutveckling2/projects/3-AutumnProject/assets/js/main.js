28ba7851dd23e9ad2af8ed7c5367c668fa05b7a5
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2020-12-20 00:08:19 +0100

    final push

--- webbutveckling2/projects/3-AutumnProject/assets/js/main.js
+++ webbutveckling2/projects/3-AutumnProject/assets/js/main.js
@@ -1,5 +1,26 @@
-var menu = document.getElementsByClassName('navImg');
+// console.log("test");
 
-menu.addEventListener('click', {
-    // 
-});
+// document.writeln("test");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var menu = document.getElementsByClassName('navImg');
+
+// menu.addEventListener('click', {
+//     // 
+// });
