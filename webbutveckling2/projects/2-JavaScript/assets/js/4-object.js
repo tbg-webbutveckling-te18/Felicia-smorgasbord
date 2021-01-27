40dcbb2a03a32d2e06582bb41409543c7cf47e2b
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2021-01-27 11:56:20 +0100

    worked with objects in js

--- webbutveckling2/projects/2-JavaScript/assets/js/4-object.js
+++ webbutveckling2/projects/2-JavaScript/assets/js/4-object.js
@@ -1,8 +1,11 @@
-
-
 var person = ['Sid', 35, 'EagleSheildBay'];
 
+document.writeln('<br>');
+document.writeln('<br>');
+document.writeln('<br>');
+
 document.writeln(person[2]);
+
 
 var personFriends = ['Bob', 'Mei', 'Alice', 'Naoko'];
 
@@ -13,9 +16,43 @@ var user = {
     friends: ['Bob', 'Mei', 'Alice', 'Naoko'],
     isEvil: false,
     shoutout: function() {
-        alert(`Sup.`);
+        alert(`Sup.`); 
     }
 };
 
-document.writeln(`My name is ${user.name} and I am ${user.age} and I live in ${user.city}`);
-user.shoutout();
+document.writeln(`My name is ${user.name}. I am ${user.age} years old and I live in ${user.city}`);
+// user.shoutout();
+
+// for(var i = 0; i < user.friends.length; i++) {
+//     console.log(user.friends[i]);
+// }
+
+user.friends.forEach(function(friend) {
+    console.log(friend)
+})
+
+console.log(user.age)
+user.age++;
+console.log(user.age)
+
+var posts = [
+    {
+        author: "Yukio",
+        comment: "Cats the movie sucks!",
+    },
+    {
+        author: "Hachi",
+        comment: "Fuck you!"
+    },
+    {
+        author: "Kamala",
+        comment: "Hey bud there is no need for such foul language."
+    }
+]
+
+// html ref
+var postOutput = document.querySelector(".posts");
+
+posts.forEach(function(post) {
+    postOutput.innerHTML += `Author: ${post.author} <br> Commented: ${post.comment}<br> <br>`;
+})
