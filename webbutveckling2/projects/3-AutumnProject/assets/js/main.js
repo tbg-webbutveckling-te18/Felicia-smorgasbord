11a18f5dd0c4a14117ae60a6c69c56038ba1f028
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2021-05-12 12:00:38 +0200

    continued with ht proj

--- webbutveckling2/projects/3-AutumnProject/assets/js/main.js
+++ webbutveckling2/projects/3-AutumnProject/assets/js/main.js
@@ -1,20 +1,22 @@
 
 
 
- 
 
 
 
- 
+
+
+
+
+//  if(button == this){
+//      document.writeln("do this");
+//  }else {
+//      document.writeln("do thit");
+//  }
 
 // console.log("test");
 
 // document.writeln("test");
-
-
-
-
-
 
 
 
